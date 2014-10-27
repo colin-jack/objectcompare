7561e785dba018a577730f64ffff14ff20f925d4
Colin Jack <colin.jack@gmail.com>
2014-10-27 17:17:41 +0000

    Small tweaks.

--- spec/testFixture.js
+++ spec/testFixture.js
@@ -1,6 +1,6 @@
 // A namespace is used so that reorganising the folder containing the code under test doesn't result in 
 // lots of broken tests, and so if we have deeply neds test files we don't need to do things like ./../../../lib
 var requireNamespace = require('require-namespace');
-module.exports = global.lib = requireNamespace.createSync('lib', __dirname + '/../lib/');
+module.exports = global.lib = requireNamespace.createSync(__dirname + '/../lib/', 'lib');
 
 global.assert = require('chai').assert;
